cccad31c0ba5366495c9d634ad40f4c73c123e6d
Jami Hyvärinen <jami@Testi-MBA.local>
2023-12-25 17:36:33 +0200

    created the info section to be rendered with js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const firstPlayerGrid = document.querySelector(".firstPlayerGrid");
 const secondPlayerGrid = document.querySelector(".secondPlayerGrid");
-
-class DOMmanipulation {
+const gameInfo = document.querySelector(".gameInfo");
+class cDOMManipulation {
   constructor() {
     this.gameGridSize = 8;
   }
 
   createPlyerGrids() {
-    for (let x = 0; x < this.gameGridSize; x++) {
-      for (let y = 0; y < this.gameGridSize; y++) {
-        // Creating grid for first player
+    for (let y = 0; y < this.gameGridSize; y++) {
+      for (let x = 0; x < this.gameGridSize; x++) {
+        const yCoord = y;
+        const xCoord = x;
 
+        // Creating grid for first player
         const firstGridCell = document.createElement("div");
         firstGridCell.classList.add("gameCell");
         firstPlayerGrid.appendChild(firstGridCell);
@@ -25,21 +27,113 @@ class DOMmanipulation {
         firstGridCell.addEventListener("click", (x, y) => {
           //ONCLICK EVENT HERE
         });
-
-        firstGridCell.innerText = "O";
-        secondGridCell.innerText = "X";
       }
     }
   }
+  renderGameInfoSection(playerOne, playerTwo) {
+    gameInfo.innerHTML = "";
+
+    // Create main info Container
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("infoContainer");
+    gameInfo.appendChild(infoContainer);
+
+    // render each information
+    this.renderShotsFiredInfo(playerOne, playerTwo, infoContainer);
+    this.renderShotsHitInfo(playerOne, playerTwo, infoContainer);
+    this.renderAccuracityInfo(playerOne, playerTwo, infoContainer);
+
+    // make div for buttons
+    const gameControlButtons = document.createElement("div");
+    gameControlButtons.classList.add("gameControlButtons");
+    gameInfo.appendChild(gameControlButtons);
+
+    this.renderEndTurnBtn(gameControlButtons);
+    this.renderResetGameBtn(gameControlButtons);
+    this.renderEndGameBtn(gameControlButtons);
+  }
+
+  renderShotsFiredInfo(playerOne, playerTwo, infoContainer) {
+    // Create shotCount into InfoContainer
+    const shotCount = document.createElement("div");
+    shotCount.classList.add("infoCell");
+    infoContainer.appendChild(shotCount);
+    //title for shotcount
+    const titleShotCount = document.createElement("div");
+    titleShotCount.classList.add("title");
+    titleShotCount.innerText = "Shots Fired";
+    shotCount.appendChild(titleShotCount);
+    //info for shotcount
+    const infoShotCount = document.createElement("div");
+    infoShotCount.classList.add("info");
+    infoShotCount.innerText =
+      playerOne.shotsFired + " - " + playerTwo.shotsFired;
+    shotCount.appendChild(infoShotCount);
+  }
+  renderShotsHitInfo(playerOne, playerTwo, infoContainer) {
+    // Create infoCell
+    const infoCell = document.createElement("div");
+    infoCell.classList.add("infoCell");
+    infoContainer.appendChild(infoCell);
+    // Create Title Container
+    const title = document.createElement("div");
+    title.classList.add("title");
+    title.innerText = "Shots Hit";
+    infoCell.appendChild(title);
+    // Create Info Container
+    const info = document.createElement("div");
+    info.classList.add("info");
+    info.innerText = playerOne.shotsHit + " - " + playerTwo.shotsHit;
+    infoCell.appendChild(info);
+  }
+  renderAccuracityInfo(playerOne, playerTwo, infoContainer) {
+    // Create infoCell
+    const infoCell = document.createElement("div");
+    infoCell.classList.add("infoCell");
+    infoContainer.appendChild(infoCell);
+    // Create Title Container
+    const title = document.createElement("div");
+    title.classList.add("title");
+    title.innerText = "Accuracity";
+    infoCell.appendChild(title);
+    // Create Info Container
+    const info = document.createElement("div");
+    info.classList.add("info");
+    info.innerText = playerOne.accuracity + " - " + playerTwo.accuracity;
+    infoCell.appendChild(info);
+  }
+  renderEndTurnBtn(gameControlButtons) {
+    const button = document.createElement("div");
+    button.innerText = "End Turn";
+    button.classList.add("endTurnBtn");
+    gameControlButtons.appendChild(button);
+  }
+  renderResetGameBtn(gameControlButtons) {
+    const button = document.createElement("div");
+    button.innerText = "Reset";
+    button.classList.add("resetGameBtn");
+    gameControlButtons.appendChild(button);
+  }
+  renderEndGameBtn(gameControlButtons) {
+    const button = document.createElement("div");
+    button.innerText = "End Game";
+    button.classList.add("endGameBtn");
+    gameControlButtons.appendChild(button);
+  }
 }
-const DOMmanipulator = new DOMmanipulation();
-DOMmanipulator.createPlyerGrids();
+const DOMManipulation = new cDOMManipulation();
 
 class Boat {
   constructor() {
     this.lenght = 4;
     this.hits = 0;
     this.sunk = false;
+    this.location = [
+      [0, 1],
+      [0, 2],
+      [0, 3],
+      [0, 4],
+    ];
   }
 
   // function for when shotted hit hits this boat
@@ -60,12 +154,21 @@ class Boat {
   }
 }
 
-class player {
-  constructor() {
-    this.boats = {
-      carrier: new Boat(),
-      submarine: new Boat(),
-      cruiser: new Boat(),
-    };
+class Player {
+  constructor(name) {
+    this.shotsFired = 0;
+    this.shotsHit = 0;
+    this.accuracity = this.shotsFired / this.shotsHit;
+    this.name = name;
+    this.boats = [{ cruiser: new Boat() }, { submarine: new Boat() }];
   }
 }
+
+function mainGame() {
+  DOMManipulation.createPlyerGrids();
+  let playerOne = new Player();
+  let playerTwo = new Player();
+
+  DOMManipulation.renderGameInfoSection(playerOne, playerTwo);
+}
+mainGame();

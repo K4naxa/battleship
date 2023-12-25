c5c61243240415ce95417dde7d17148217ae5a1c
Jami Hyvärinen <jami@Testi-MBA.local>
2023-12-25 16:13:29 +0200

    created the main layout for the site

--- script.js
+++ script.js
@@ -1,64 +1,56 @@
 const firstPlayerGrid = document.querySelector(".firstPlayerGrid");
 const secondPlayerGrid = document.querySelector(".secondPlayerGrid");
 
+class DOMmanipulation {
+  constructor() {
+    this.gameGridSize = 8;
+  }
 
-class DOMmanipulation{
-    constructor(){
-        this.gameGridSize = 8;
+  createPlyerGrids() {
+    for (let x = 0; x < this.gameGridSize; x++) {
+      for (let y = 0; y < this.gameGridSize; y++) {
+        // Creating grid for first player
 
-    }
+        const firstGridCell = document.createElement("div");
+        firstGridCell.classList.add("gameCell");
+        firstPlayerGrid.appendChild(firstGridCell);
 
-     createPlyerGrids() {
-        for (let x = 0; x < this.gameGridSize; x++) {
-          for (let y = 0; y < this.gameGridSize; y++) {
-            // Creating grid for first player
-      
-            const firstGridCell = document.createElement("div");
-            firstGridCell.classList.add("gameCell");
-            firstPlayerGrid.appendChild(firstGridCell);
+        // Creating grid for second player
+        const secondGridCell = document.createElement("div");
+        secondGridCell.classList.add("gameCell");
+        secondPlayerGrid.appendChild(secondGridCell);
 
-      
-            // Creating grid for second player
-            const secondGridCell = document.createElement("div");
-            secondGridCell.classList.add("gameCell");
-            secondPlayerGrid.appendChild(secondGridCell);
-      
-            // Add click funtion for this cell
-      
-            firstGridCell.addEventListener("click", (x, y) => {
+        // Add click funtion for this cell
 
-                //ONCLICK EVENT HERE
-            });
+        firstGridCell.addEventListener("click", (x, y) => {
+          //ONCLICK EVENT HERE
+        });
 
-
-            firstGridCell.innerText = x + " , " + y
-            secondGridCell.innerText = x + " , " + y
-
-          }
-        }
+        firstGridCell.innerText = "O";
+        secondGridCell.innerText = "X";
       }
+    }
+  }
 }
-const DOMmanipulator = new DOMmanipulation;
-DOMmanipulator.createPlyerGrids()
-
+const DOMmanipulator = new DOMmanipulation();
+DOMmanipulator.createPlyerGrids();
 
 class Boat {
-    constructor(){
-        this.lenght = 4
-        this.hits = 0;
-        this.sunk = false;
-    }
+  constructor() {
+    this.lenght = 4;
+    this.hits = 0;
+    this.sunk = false;
+  }
 
-    // function for when shotted hit hits this boat
+  // function for when shotted hit hits this boat
   hit() {
     // if already hit there > ignore
     if ((this.sunk = true)) return;
 
+    // CREATE LOGIC FOR CHECKING IF HIT AT THE SAME PLACE AND REPORT THE DMG
 
-    // CREATE LOGIC FOR CHECKING IF HIT AT THE SAME PLACE AND REPORT THE DMG 
-
-    if (this.lenght == this.hits){
-        this.boatSunk()
+    if (this.lenght == this.hits) {
+      this.boatSunk();
     }
   }
 
@@ -68,16 +60,12 @@ class Boat {
   }
 }
 
-class player{
-    constructor(){
-        this.boats = {
-            carrier : new Boat,
-            submarine : new Boat,
-            cruiser : new Boat,
-        }
-    }
-
-
-
-
+class player {
+  constructor() {
+    this.boats = {
+      carrier: new Boat(),
+      submarine: new Boat(),
+      cruiser: new Boat(),
+    };
+  }
 }
